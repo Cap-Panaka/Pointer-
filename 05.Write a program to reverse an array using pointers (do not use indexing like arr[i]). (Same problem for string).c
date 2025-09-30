cba75a9ec609678793e8b5560bf7f6612574b6b9
Cap-Panaka <nerdcrab1122@gmail.com>
2025-09-30 10:53:04 +0600

    Update 05.Write a program to reverse an array using pointers (do not use indexing like arr[i]). (Same problem for string).c

--- 05.Write a program to reverse an array using pointers (do not use indexing like arr[i]). (Same problem for string).c
+++ 05.Write a program to reverse an array using pointers (do not use indexing like arr[i]). (Same problem for string).c
@@ -1,4 +1,8 @@
-/*05.Write a program to reverse an array using pointers (do not use indexing like arr[i]). (Same problem for string)*/
+/*05.Write a program to reverse an array using pointers (do not use indexing like arr[i]). (Same problem for string)
+How many elements you want to input: 5
+Enter elements: 1 2 23 4 5
+After reversing: 5 4 23 2 1 
+*/
 
 
 //Using Array:
@@ -22,4 +26,34 @@ int main()
       printf("%d ",*(p+i));
    }
 }
+
+
+//Another Way
+#include<stdio.h>
+int main()
+{
+    int n,i,*p,*p1;
+    printf("How many elements you want to enter: ");
+    scanf("%d",&n);
+    printf("Enter elements: ");
+    int ar[n],ar1[n];
+    p=ar;
+    p1=ar1;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",p+i);
+    }
+    
+    for(i=0;i<n;i++)
+    {
+        *(p1+i)=*(p+(n-1-i));
+    }
+    
+    printf("After reversing: ");
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",*(p1+i));
+    }
+}
+
 
